7c8f6bb3a038fea836dc6408a25499f4d41132ec
Joel Sleppy <joel@sleppytech.com>
2025-08-13 15:10:09 -0400

    Bump djp dependency to >=0.3.1 (#5)

--- tests/test_django_plugin_extensions.py
+++ tests/test_django_plugin_extensions.py
@@ -7,6 +7,6 @@ class TestDjangoPluginExtensions(TestCase):
     def test_model_extensions_available(self):
         self.assertEqual(TestModel.objects.count(), 0)
         TestModel.objects.create(title="Test Title", description="Test Description")
-        instance = TestModel.objects.first()
+        instance = TestModel.objects.get()
         self.assertEqual(instance.title, "Test Title")
         self.assertEqual(instance.description, "Test Description")
